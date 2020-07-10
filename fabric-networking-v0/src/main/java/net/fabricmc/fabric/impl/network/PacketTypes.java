6bca7e642ba47ec13549530b758d85cf16f3b65d
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-07-10 15:46:17 +0530

    Created packet registry implementation
     Changes to be committed:
    	new file:   fabric-networking-v0/src/main/java/net/fabricmc/fabric/impl/network/PacketDebugOptions.java
    	new file:   fabric-networking-v0/src/main/java/net/fabricmc/fabric/impl/network/PacketRegistryImpl.java
    	new file:   fabric-networking-v0/src/main/java/net/fabricmc/fabric/impl/network/PacketTypes.java

---
+++ fabric-networking-v0/src/main/java/net/fabricmc/fabric/impl/network/PacketTypes.java
@@ -0,0 +1,9 @@
+package net.fabricmc.fabric.impl.network;
+
+public class PacketTypes {
+	public static final String BRAND = "minecraft:brand";
+	public static final String REGISTER = "minecraft:register";
+	public static final String UNREGISTER = "minecraft:unregister";
+
+	public static final String OPEN_CONTAINER = "fabric:container/open";
+}
