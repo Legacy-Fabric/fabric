c997e66ca043cfe7c575dc6416e3eb4bbdec6ba4
shedaniel <daniel@shedaniel.me>
2020-07-14 18:38:53 +0800

    key-binding-api-v1 and test mods (#12)
    * key-binding-api-v1 and test mods
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * fix checkstyle
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- fabric-key-binding-api-v1/src/main/java/net/fabricmc/fabric/mixin/client/keybinding/MixinGameOptions.java
+++ fabric-key-binding-api-v1/src/main/java/net/fabricmc/fabric/mixin/client/keybinding/MixinGameOptions.java
@@ -14,26 +14,26 @@
  * limitations under the License.
  */
 
-package net.fabricmc.fabric.mixin.content.registries;
+package net.fabricmc.fabric.mixin.client.keybinding;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.block.entity.FurnaceBlockEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.options.KeyBinding;
 
-import net.fabricmc.fabric.api.content.registry.v1.FuelRegistry;
+import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
 
-@Mixin(FurnaceBlockEntity.class)
-public class MixinFurnaceBlockEntity {
-	@Inject(at = @At("HEAD"), method = "method_1144", cancellable = true)
-	private static void registerFuels(ItemStack stack, CallbackInfoReturnable<Integer> info) {
-		Integer value = FuelRegistry.INSTANCE.getFuelMap().get(stack.getItem());
+@Mixin(GameOptions.class)
+public class MixinGameOptions {
+	@Shadow
+	public KeyBinding[] keysAll;
 
-		if (value != null) {
-			info.setReturnValue(value);
-		}
+	@Inject(at = @At("HEAD"), method = "method_2336()V")
+	public void loadHook(CallbackInfo info) {
+		keysAll = KeyBindingRegistryImpl.process(keysAll);
 	}
 }
