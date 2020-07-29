8723bc17d5464cebe2cdc12ef1526d35d44d0126
SD <harrypotter.starwars.shrish@gmail.com>
2020-07-29 13:02:11 +0530

    bers
     On branch feature/renderingv1
     Changes to be committed:
    	new file:   fabric-rendering-v1/src/main/java/net/fabricmc/fabric/api/client/render/BlockEntityRendererRegistry.java

---
+++ fabric-rendering-v1/src/main/java/net/fabricmc/fabric/api/client/render/BlockEntityRendererRegistry.java
@@ -0,0 +1,45 @@
+package net.fabricmc.fabric.api.client.render;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+
+public class BlockEntityRendererRegistry {
+	public static final BlockEntityRendererRegistry INSTANCE = new BlockEntityRendererRegistry();
+	private Map<Class<? extends BlockEntity>, BlockEntityRenderer<? extends BlockEntity>> renderers = null;
+	private Map<Class<? extends BlockEntity>, BlockEntityRenderer<? extends BlockEntity>> renderersTmp = new HashMap<>();
+
+	private BlockEntityRendererRegistry() {
+	}
+
+	public void initialize(BlockEntityRenderDispatcher instance, Map<Class<? extends BlockEntity>, BlockEntityRenderer<? extends BlockEntity>> map) {
+		if (renderers != null && renderers != map) {
+			throw new RuntimeException("Tried to set renderers twice!");
+		}
+
+		if (renderers == map) {
+			return;
+		}
+
+		renderers = map;
+
+		for (BlockEntityRenderer<? extends BlockEntity> renderer : renderersTmp.values()) {
+			renderer.method_3751(instance);
+		}
+
+		renderers.putAll(renderersTmp);
+		renderersTmp = null;
+	}
+
+	public void register(Class<? extends BlockEntity> blockEntityClass, BlockEntityRenderer<? extends BlockEntity> blockEntityRenderer) {
+		if (renderers != null) {
+			renderers.put(blockEntityClass, blockEntityRenderer);
+			blockEntityRenderer.method_3751(BlockEntityRenderDispatcher.INSTANCE);
+		} else {
+			renderersTmp.put(blockEntityClass, blockEntityRenderer);
+		}
+	}
+}
