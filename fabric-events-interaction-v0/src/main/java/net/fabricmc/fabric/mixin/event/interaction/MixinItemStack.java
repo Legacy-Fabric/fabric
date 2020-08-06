6461d4a7a7a03e5229d0e2f0decf7a8e34f300b5
SD <62552372+BoogieMonster1O1@users.noreply.github.com>
2020-08-06 17:17:44 +0530

    Block place and Block break events (#23)
    * Added BreakBlockCallback
    
     Changes to be committed:
    	new file:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BreakBlockCallback.java
    	modified:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinServerPlayerInteractionManager.java
    
    * Added PlaceBlockCallback
    
     Changes to be committd:
    	new file:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlaceBlockCallback.java
    	new file:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemStack.java
    	modified:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinServerPlayerInteractionManager.java
    	modified:   fabric-events-interaction-v0/src/main/resources/fabric-events-interaction-v0.mixins.json
    
    * BUmpedversions
    
     Changes to be committed:
    	modified:   build.gradle
    	modified:   fabric-events-interaction-v0/build.gradle
    
    * Checkstyle
    
     Changes to be committed:
    	modified:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlaceBlockCallback.java
    	modified:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemStack.java

---
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemStack.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.event.interaction;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+import net.fabricmc.fabric.api.event.player.PlaceBlockCallback;
+import net.fabricmc.fabric.impl.base.util.ActionResult;
+
+@Mixin(ItemStack.class)
+public class MixinItemStack {
+	@Inject(at = @At("HEAD"), method = "method_8347", cancellable = true)
+	public void blockPlaced (PlayerEntity playerEntity, World world, BlockPos blockPos, Direction direction, float hitX, float hitY, float hitZ, CallbackInfoReturnable<Boolean> info) {
+		ActionResult result = PlaceBlockCallback.EVENT.invoker().blockPlaced(playerEntity, world, blockPos, direction, hitX, hitY, hitZ);
+
+		if (result == ActionResult.FAIL) {
+			info.setReturnValue(false);
+		}
+	}
+}
