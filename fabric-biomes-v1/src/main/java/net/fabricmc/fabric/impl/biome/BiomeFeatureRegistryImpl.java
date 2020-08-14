d912dd4824ed2f4bfe0612817f03262f9007622a
SD <harrypotter.starwars.shrish@gmail.com>
2020-08-14 14:26:18 +0530

    Added biomes module
     Changes to be committed:
    	new file:   fabric-biomes-v1/build.gradle
    	new file:   fabric-biomes-v1/src/main/java/net/fabricmc/fabric/api/biome/v1/BiomeFeatureRegistry.java
    	new file:   fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biome/BiomeFeatureRegistryImpl.java
    	new file:   fabric-biomes-v1/src/main/resources/assets/fabric-biomes-v1/icon.png
    	new file:   fabric-biomes-v1/src/main/resources/fabric-biomes-v1.mixins.json
    	new file:   fabric-biomes-v1/src/main/resources/fabric.mod.json
    	modified:   settings.gradle

---
+++ fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biome/BiomeFeatureRegistryImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.impl.biome;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeature;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeFeatureRegistry;
+
+public class BiomeFeatureRegistryImpl implements BiomeFeatureRegistry {
+	@Override
+	public void registerOreFeature(OreFeature ore, int count, int minHeight, int maxHeight, Biome... restrictionBiomes) {
+
+	}
+
+	@Override
+	public void registerFeature(Feature feature, Biome... restrictionBiomes) {
+
+	}
+}
