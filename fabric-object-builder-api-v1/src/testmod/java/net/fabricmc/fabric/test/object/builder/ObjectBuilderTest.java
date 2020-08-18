e14478d052f8839df22a2d3be4ab7a508dbfbcac
BoogieMonster1O1 <62552372+BoogieMonster1O1@users.noreply.github.com>
2020-08-18 15:16:18 +0530

    Material Builder (#27)
    * Added object builder api module
    
     Changes to be committed:
    	new file:   fabric-object-builder-api-v1/build.gradle
    	new file:   fabric-object-builder-api-v1/src/main/resources/assets/fabric-object-builder-api-v1/icon.png
    	new file:   fabric-object-builder-api-v1/src/main/resources/fabric-object-builder-api-v1.mixins.json
    	new file:   fabric-object-builder-api-v1/src/main/resources/fabric.mod.json
    	modified:   settings.gradle
    
    * Added FabricBlockMaterial
    
     Changes to be committed:
    	new file:   fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/api/object/builder/v1/FabricBlockMaterial.java
    	new file:   fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/mixin/object/builder/MaterialAccessor.java
    	modified:   fabric-object-builder-api-v1/src/main/resources/fabric-object-builder-api-v1.mixins.json
    
    * Added a testmod
    
    no I am not making a block. don't even ask. yes I know you were gonna tell me to do it.
    
     Changes to be committed:
    	modified:   fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/api/object/builder/v1/FabricBlockMaterial.java
    	new file:   fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/ObjectBuilderTest.java
    	new file:   fabric-object-builder-api-v1/src/testmod/resources/fabric.mod.json
    
    * Bump version
    
     Changes to be committed:
    	modified:   build.gradle
    
    * Renamed package
    
     Changes to be committed:
    	renamed:    fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/api/object/builder/v1/FabricBlockMaterial.java -> fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/api/object/builder/v1/block/FabricBlockMaterial.java
    	modified:   fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/ObjectBuilderTest.java
    
    * Added a block to the testmod
    
    should probably go in the non existant content registries testmod but okay
    
     Changes to be committed:
    	modified:   fabric-object-builder-api-v1/build.gradle
    	modified:   fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/api/object/builder/v1/block/FabricBlockMaterial.java
    	modified:   fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/ObjectBuilderTest.java
    	new file:   fabric-object-builder-api-v1/src/testmod/resources/assets/fabric-object-builder-api-v1-testmod/blockstates/test_block.json
    
    * Bumped version
    
     Changes to be committed:
    	modified:   build.gradle
    
    * Bump version to 0.6.0
    
    Co-authored-by: shedaniel <daniel@shedaniel.me>

---
+++ fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/ObjectBuilderTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.object.builder;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
+import net.minecraft.util.Identifier;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.content.registry.v1.BlockRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockMaterial;
+
+public class ObjectBuilderTest implements ModInitializer {
+	public static final Material TEST_MATERIAL = new FabricBlockMaterial(MaterialColor.WHITE)
+			.requiresSilkTouch()
+			.setCanBeBrokenInAdventureMode()
+			.doesNotBlockMovement()
+			.noCollision()
+			.setFlammable();
+	public static final Block TEST_BLOCK = BlockRegistry.register(new Identifier("fabric-object-builder-api-v1-testmod", "test_block"), new Block(TEST_MATERIAL, MaterialColor.AIR));
+
+	@Override
+	public void onInitialize() {
+	}
+}
