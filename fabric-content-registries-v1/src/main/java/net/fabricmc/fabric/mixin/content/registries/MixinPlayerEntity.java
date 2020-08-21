732c40216b0249d87c7d3f6bde0a0688372e9eee
BoogieMonster1O1 <62552372+BoogieMonster1O1@users.noreply.github.com>
2020-08-21 11:11:47 +0530

    Two more content registry interfaces (#34)
    * Added a seperate package for interfaces and added UseTickable
    
     Changes to be committed:
    	renamed:    fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/Climbable.java -> fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/block/Climbable.java
    	new file:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/item/UseTickable.java
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/mixin/content/registries/MixinLivingEntity.java
    	new file:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/mixin/content/registries/MixinPlayerEntity.java
    	modified:   fabric-content-registries-v1/src/main/resources/fabric-content-registries-v1.mixins.json
    	modified:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemStack.java
    
    * fabricmc.net -> legacyfabric.github.io
    
     Changes to be committed:
    	modified:   fabric-api-base/src/main/resources/fabric.mod.json
    	modified:   fabric-armor-api-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-commands-v0/src/main/resources/fabric.mod.json
    	modified:   fabric-content-registries-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-crash-report-info-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-events-interaction-v0/src/main/resources/fabric.mod.json
    	modified:   fabric-events-lifecycle-v0/src/main/resources/fabric.mod.json
    	modified:   fabric-gamerule-api-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-item-groups-v0/src/main/resources/fabric.mod.json
    	modified:   fabric-key-binding-api-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-lifecycle-events-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-networking-v0/src/main/resources/fabric.mod.json
    	modified:   fabric-object-builder-api-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-recipes-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-rendering-v1/src/main/resources/fabric.mod.json
    	modified:   fabric-resource-loader-v0/src/main/resources/fabric.mod.json
    
    * Added a block break listener
    
     Changes to be committed:
    	new file:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/block/PlayerBlockBreakListener.java
    	new file:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/mixin/content/registries/MixinServerPlayerInteractionManager.java
    	modified:   fabric-content-registries-v1/src/main/resources/fabric-content-registries-v1.mixins.json
    
    * Kekstyle
    
     Changes to be committed:
    	modified:   build.gradle
    	modified:   fabric-content-registries-v1/build.gradle
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/block/PlayerBlockBreakListener.java
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/item/UseTickable.java
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/mixin/content/registries/MixinPlayerEntity.java
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/mixin/content/registries/MixinServerPlayerInteractionManager.java
    	modified:   fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemStack.java
    
    * bump heverytheeng
    
    * annotate
    
     Changes to be committed:
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/PlayerBlockHarvestListener.java
    	modified:   fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/api/content/registry/v1/UseTickable.java

---
+++ fabric-content-registries-v1/src/main/java/net/fabricmc/fabric/mixin/content/registries/MixinPlayerEntity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.content.registries;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+
+import net.fabricmc.fabric.api.content.registry.v1.UseTickable;
+
+@Mixin(PlayerEntity.class)
+public class MixinPlayerEntity {
+	@Shadow
+	public PlayerInventory inventory;
+
+	@Shadow
+	private ItemStack field_8356;
+
+	@Shadow
+	private int field_8357;
+
+	@Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/player/PlayerInventory;getMainHandStack()Lnet/minecraft/item/ItemStack;"), method = "tick")
+	public void useTick(CallbackInfo ci) {
+		if (this.inventory.getMainHandStack() == this.field_8356 && this.field_8356.getItem() instanceof UseTickable && this.field_8357 > 0) {
+			((UseTickable) this.field_8356.getItem()).useTick(this.field_8356, (PlayerEntity) (Object) this, this.field_8357);
+		}
+	}
+}
