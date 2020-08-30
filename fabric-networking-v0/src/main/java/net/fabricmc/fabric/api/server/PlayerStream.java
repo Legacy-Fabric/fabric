ff5edcd362eb6ae773b73a38565facf52f80d588
SD <harrypotter.starwars.shrish@gmail.com>
2020-08-30 16:59:16 +0530

    Implemented watching and around
     Changes to be committed:
    	modified:   fabric-networking-v0/src/main/java/net/fabricmc/fabric/api/server/PlayerStream.java

--- fabric-networking-v0/src/main/java/net/fabricmc/fabric/api/server/PlayerStream.java
+++ fabric-networking-v0/src/main/java/net/fabricmc/fabric/api/server/PlayerStream.java
@@ -18,10 +18,13 @@ package net.fabricmc.fabric.api.server;
 
 import java.util.stream.Stream;
 
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public final class PlayerStream {
@@ -42,5 +45,27 @@ public final class PlayerStream {
 		}
 
 		return ((ServerWorld) world).playerEntities.stream();
+	}
+
+	public static Stream<PlayerEntity> watching(BlockEntity entity) {
+		return watching(entity.getWorld(), entity.getPos());
+	}
+
+	public static Stream<PlayerEntity> watching(World world, BlockPos pos) {
+		if (!(world instanceof ServerWorld)) {
+			throw new RuntimeException("Only supported on ServerWorld!");
+		}
+
+		return world(world).filter((player -> ((ServerWorld) world).getChunkManager().method_6005((ServerPlayerEntity) player, pos.getX(), pos.getZ())));
+	}
+
+	public static Stream<PlayerEntity> around(World world, Vec3d vector, double radius) {
+		double radiusSq = radius * radius;
+		return world(world).filter((p) -> p.getDistanceTo(vector.x, vector.y, vector.z) <= radiusSq);
+	}
+
+	public static Stream<PlayerEntity> around(World world, BlockPos pos, double radius) {
+		double radiusSq = radius * radius;
+		return world(world).filter((p) -> p.getDistanceTo(pos.getX(), pos.getY(), pos.getZ()) <= radiusSq);
 	}
 }
