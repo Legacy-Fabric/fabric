c72f768745f27fd662a2fd756bec8f583d5774c0
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-17 16:12:22 +0530

    Get rid of it
     Changes to be committed:
    	deleted:    fabric-biomes-v1/build.gradle
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/api/biomes/v1/Climate.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/api/biomes/v1/FabricBiome.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/api/biomes/v1/OverworldBiomes.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biomes/InternalBiomeUtils.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biomes/LayerRandom.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biomes/VariantTransformer.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biomes/WeightedBiomeEntry.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biomes/WeightedPicker.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/mixin/biomes/LakesFeatureAccessor.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/mixin/biomes/MixinLayer.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/mixin/biomes/MixinSetBaseBiomesLayer.java
    	deleted:    fabric-biomes-v1/src/main/java/net/fabricmc/fabric/mixin/biomes/MixinSurfaceChunkGenerator.java
    	deleted:    fabric-biomes-v1/src/main/resources/assets/fabric-biomes-v1/icon.png
    	deleted:    fabric-biomes-v1/src/main/resources/fabric-biomes-v1.mixins.json
    	deleted:    fabric-biomes-v1/src/main/resources/fabric.mod.json
    	modified:   settings.gradle

--- fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biomes/LayerRandom.java
+++
@@ -1,8 +0,0 @@
-package net.fabricmc.fabric.impl.biomes;
-
-/**
- * Interface for invoking the protected method nextInt() on various layers.
- */
-public interface LayerRandom {
-	int nextIntAccess(int bound);
-}
