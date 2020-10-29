f5e3440707b5c97ffb33a934f4e97192773171c8
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-29 12:01:24 +0530

    Assertive
     Changes to be committed:
    	modified:   fabric-sponge-command-api-v2/src/main/java/net/fabricmc/fabric/api/command/v2/PermissibleCommandSource.java

--- fabric-sponge-command-api-v2/src/main/java/net/fabricmc/fabric/api/command/v2/PermissibleCommandSource.java
+++ fabric-sponge-command-api-v2/src/main/java/net/fabricmc/fabric/api/command/v2/PermissibleCommandSource.java
@@ -19,8 +19,8 @@ package net.fabricmc.fabric.api.command.v2;
 import net.minecraft.command.CommandSource;
 
 /**
- * Represents a {@link CommandSource} that may not be able to run
- * a command as they do not have the permission to do so.
+ * Represents a {@link CommandSource} that can not be able to run
+ * a command if they do not have the permission to do so.
  */
 public interface PermissibleCommandSource extends CommandSource {
 	boolean hasPermission(String perm);
