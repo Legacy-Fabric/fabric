016db593ae27f9d0d7ae02223f0b0e4450274fbd
Luke Schwager <luke@blucobalt.dev>
2020-12-02 22:03:48 -0800

    dumb error is goen
    Signed-off-by: Luke Schwager <luke@blucobalt.dev>

--- fabric-networking-blockentity-v0/src/main/java/net/fabricmc/fabric/mixin/networking/blockentity/MixinClientPlayNetworkHandler.java
+++ fabric-networking-blockentity-v0/src/main/java/net/fabricmc/fabric/mixin/networking/blockentity/MixinClientPlayNetworkHandler.java
@@ -36,7 +36,7 @@ import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPlayNetworkHandler.class)
 public class MixinClientPlayNetworkHandler {
-	@Shadow
+	/*@Shadow
 	@Final
 	private static Logger LOGGER;
 
@@ -66,5 +66,5 @@ public class MixinClientPlayNetworkHandler {
 		} else {
 			throw new RuntimeException("Block Entity sync Id was not 127! This is a bug!");
 		}
-	}
+	}*/
 }
