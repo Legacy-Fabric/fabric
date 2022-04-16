bffacd370a38c874f6fbdaee36d535f50ed2e155
SD <harrypotter.starwars.shrish@gmail.com>
2022-04-16 20:27:47 +0530

    Syncing

--- modules/shared/registry-sync/src/main/java/net/legacyfabric/fabric/impl/client/registry/sync/ClientRemapInitializer.java
+++ modules/shared/registry-sync/src/main/java/net/legacyfabric/fabric/impl/client/registry/sync/ClientRemapInitializer.java
@@ -1,8 +1,29 @@
+/*
+ * Copyright (c) 2020 - 2021 Legacy Fabric
+ * Copyright (c) 2016 - 2021 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.legacyfabric.fabric.impl.client.registry.sync;
 
 import net.legacyfabric.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.legacyfabric.fabric.impl.registry.sync.RegistryRemapper;
 import net.legacyfabric.fabric.impl.registry.sync.RegistryRemapperAccess;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import net.minecraft.nbt.CompoundTag;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -12,8 +33,16 @@ import net.fabricmc.api.Environment;
 public class ClientRemapInitializer implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		ClientPlayNetworking.registerGlobalReceiver(RegistryRemapper.PACKET_ID, (client, handler, buf, responseSender) -> {
-			client.execute(() -> ((RegistryRemapperAccess) client.world).remap());
+		ClientPlayNetworking.registerGlobalReceiver(RegistryRemapperAccess.PACKET_ID, (client, handler, buf, responseSender) -> {
+			CompoundTag tag;
+			try {
+				tag = buf.readCompoundTag();
+			} catch (IOException e) {
+				throw new UncheckedIOException(e);
+			}
+			client.execute(() -> {
+				((RegistryRemapperAccess) client.world).readAndRemap(tag);
+			});
 		});
 	}
 }
--- modules/shared/registry-sync/src/main/java/net/legacyfabric/fabric/impl/registry/sync/RegistryRemapperAccess.java
+++ modules/shared/registry-sync/src/main/java/net/legacyfabric/fabric/impl/registry/sync/RegistryRemapperAccess.java
@@ -1,8 +1,31 @@
+/*
+ * Copyright (c) 2020 - 2021 Legacy Fabric
+ * Copyright (c) 2016 - 2021 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.legacyfabric.fabric.impl.registry.sync;
 
+import net.legacyfabric.fabric.api.networking.v1.PacketByteBufs;
+
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
 
 public interface RegistryRemapperAccess {
+	Identifier PACKET_ID = new Identifier("legacy-fabric-api", "registry_remap");
+
 	RegistryRemapper getItemRemapper();
 
 	RegistryRemapper getBlockRemapper();
@@ -23,5 +46,11 @@ public interface RegistryRemapperAccess {
 		tag.put("Items", this.getItemRemapper().toTag());
 		tag.put("Blocks", this.getBlockRemapper().toTag());
 		return tag;
+	}
+
+	default PacketByteBuf createBuf() {
+		PacketByteBuf buf = PacketByteBufs.create();
+		buf.writeCompoundTag(this.toRegistryTag());
+		return buf;
 	}
 }
