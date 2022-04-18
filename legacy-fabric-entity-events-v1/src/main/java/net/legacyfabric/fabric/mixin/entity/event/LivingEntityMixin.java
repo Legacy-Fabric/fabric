a9c9aa193029c283ab983d6d649240b2d6581e79
Cat Core <arthur.koalas@free.fr>
2022-04-18 21:39:51 +0200

    Style

--- legacy-fabric-entity-events-v1/src/main/java/net/legacyfabric/fabric/mixin/entity/event/LivingEntityMixin.java
+++ legacy-fabric-entity-events-v1/src/main/java/net/legacyfabric/fabric/mixin/entity/event/LivingEntityMixin.java
@@ -21,9 +21,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 
