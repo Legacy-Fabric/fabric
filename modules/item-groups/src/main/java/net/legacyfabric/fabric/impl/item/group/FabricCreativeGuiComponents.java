e1b744483ab7b3ab225080fdc4ababe9a4588e20
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-21 22:23:25 +0700

    license and checkstyle

--- modules/item-groups/src/main/java/net/legacyfabric/fabric/impl/item/group/FabricCreativeGuiComponents.java
+++ modules/item-groups/src/main/java/net/legacyfabric/fabric/impl/item/group/FabricCreativeGuiComponents.java
@@ -1,15 +1,32 @@
+/*
+ * Copyright (c) 2020 - 2021 Legacy Fabric
+ * Copyright (c) 2016 - 2021 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.legacyfabric.fabric.impl.item.group;
+
+import java.awt.Color; // TODO: for test
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.item.itemgroup.ItemGroup;
 import net.minecraft.util.Identifier;
-
-import java.awt.*;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Consumer;
 
 public class FabricCreativeGuiComponents {
 	private static final Identifier BUTTON_TEX = new Identifier("fabric", "textures/gui/creative_buttons.png");
--- modules/item-groups/src/main/java/net/legacyfabric/fabric/mixin/item/group/client/MixinCreativePlayerInventoryGui.java
+++ modules/item-groups/src/main/java/net/legacyfabric/fabric/mixin/item/group/client/MixinCreativePlayerInventoryGui.java
@@ -1,17 +1,35 @@
+/*
+ * Copyright (c) 2020 - 2021 Legacy Fabric
+ * Copyright (c) 2016 - 2021 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.legacyfabric.fabric.mixin.item.group.client;
 
 import net.legacyfabric.fabric.impl.item.group.CreativeGuiExtensions;
 import net.legacyfabric.fabric.impl.item.group.FabricCreativeGuiComponents;
-import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.item.itemgroup.ItemGroup;
-import net.minecraft.screen.ScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.item.itemgroup.ItemGroup;
+import net.minecraft.screen.ScreenHandler;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class MixinCreativePlayerInventoryGui extends InventoryScreen implements CreativeGuiExtensions {
@@ -29,13 +47,12 @@ public abstract class MixinCreativePlayerInventoryGui extends InventoryScreen im
 	private static int fabric_currentPage = 0;
 
 	private int fabric_getPageOffset(int page) {
-		switch (page) {
-			case 0:
-				return 0;
-			case 1:
-				return 11;
-			default:
-				return 11 + ((11 - FabricCreativeGuiComponents.COMMON_GROUPS.size()) * (page - 1));
+		if (page == 0) {
+			return 0;
+		} else if (page == 1) {
+			return 11;
+		} else {
+			return 11 + ((11 - FabricCreativeGuiComponents.COMMON_GROUPS.size()) * (page - 1));
 		}
 	}
 
