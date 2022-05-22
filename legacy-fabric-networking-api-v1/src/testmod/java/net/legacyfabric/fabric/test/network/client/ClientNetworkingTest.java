27fcec0ca5868ae2508a97bf2edc6fc1520a16cc
Cat Core <arthur.koalas@free.fr>
2022-05-22 19:33:19 +0200

    moved test mods

---
+++ legacy-fabric-networking-api-v1/src/testmod/java/net/legacyfabric/fabric/test/network/client/ClientNetworkingTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 - 2021 Legacy Fabric
+ * Copyright (c) 2016 - 2021 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.legacyfabric.fabric.test.network.client;
+
+import java.util.Arrays;
+
+import net.legacyfabric.fabric.api.client.networking.v1.C2SPlayChannelEvents;
+import net.legacyfabric.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.legacyfabric.fabric.api.logger.v1.Logger;
+import net.legacyfabric.fabric.impl.logger.LoggerImpl;
+
+public class ClientNetworkingTest implements ClientModInitializer {
+	private static final Logger LOGGER = Logger.get(LoggerImpl.API, "Test", "ClientNetworking");
+
+	@Override
+	public void onInitializeClient() {
+		C2SPlayChannelEvents.REGISTER.register((handler, sender, client, channels) -> {
+			LOGGER.info("Registered channels (C2S callback) - " + Arrays.toString(channels.toArray()));
+		});
+		C2SPlayChannelEvents.UNREGISTER.register((handler, sender, client, channels) -> {
+			LOGGER.info("Unregistered channels (C2S callback) - " + Arrays.toString(channels.toArray()));
+		});
+		ClientPlayConnectionEvents.INIT.register((handler, client) -> {
+			LOGGER.info("Connection initialized (C2S)");
+		});
+		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+			LOGGER.info("World joined (C2S)");
+		});
+		ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+			LOGGER.info("Connection disconnected (C2S)");
+		});
+	}
+}
---
+++ legacy-fabric-rendering-api-v1/src/testmod/java/net/legacyfabric/fabric/test/client/rendering/RenderingEventsTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020 - 2021 Legacy Fabric
+ * Copyright (c) 2016 - 2021 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.legacyfabric.fabric.test.client.rendering;
+
+import java.awt.Color;
+
+import net.legacyfabric.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.legacyfabric.fabric.api.client.rendering.v1.InvalidateRenderStateCallback;
+
+import net.legacyfabric.fabric.api.logger.v1.Logger;
+import net.legacyfabric.fabric.impl.logger.LoggerImpl;
+import net.minecraft.client.resource.language.I18n;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RenderingEventsTest implements ClientModInitializer {
+	private static final Logger LOGGER = Logger.get(LoggerImpl.API, "Test", "RenderingEvents");
+
+	@Override
+	public void onInitializeClient() {
+		HudRenderCallback.EVENT.register((client, tickDelta) -> {
+			client.textRenderer.draw(I18n.translate("legacyfabric.api.youreTesting"), 10, 10, Color.RED.getRGB());
+		});
+		InvalidateRenderStateCallback.EVENT.register(() -> {
+			LOGGER.info("Render state invalidated");
+		});
+	}
+}
