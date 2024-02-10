b38294a2ddda4ec086ebaf5d4e7ac459a8877db9
Cat Core <arthur.koalas@free.fr>
2024-02-10 16:55:17 +0100

    Test custom keybinding categories

--- legacy-fabric-keybindings-api-v1/1.7.10/src/testmod/java/net/legacyfabric/fabric/test/client/keybinding/KeybindingTest.java
+++ legacy-fabric-keybindings-api-v1/1.7.10/src/testmod/java/net/legacyfabric/fabric/test/client/keybinding/KeybindingTest.java
@@ -29,7 +29,7 @@ import net.legacyfabric.fabric.api.client.keybinding.v1.KeyBindingHelper;
 public class KeybindingTest implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("api.keybinding.testTranslationKey", Keyboard.KEY_F, "key.categories.misc"));
+		KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("api.keybinding.testTranslationKey", Keyboard.KEY_F, "key.categories.lftesting"));
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (keyBinding.wasPressed()) {
 				System.out.printf("The key %s was pressed%n", Keyboard.getKeyName(keyBinding.getCode()));
