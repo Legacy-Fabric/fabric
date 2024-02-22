67279df8648ab2dc0a2526463681a0d2bcc34f01
Cat Core <34719527+thecatcore@users.noreply.github.com>
2024-02-22 18:08:48 +0100

    Fix crash on startup on MC1.7.10 (#162)
    * Closes Legacy-Fabric/fabric#161
    
    * Fix typo in package name

--- legacy-fabric-networking-api-v1/common/src/main/java/net/legacyfabric/fabric/mixin/networking/client/ClientPlayNetworkHandlerMixin.java
+++ legacy-fabric-networking-api-v1/common/src/main/java/net/legacyfabric/fabric/mixin/networking/client/ClientPlayNetworkHandlerMixin.java
@@ -47,8 +47,8 @@ abstract class ClientPlayNetworkHandlerMixin implements ClientPlayNetworkHandler
 	@Unique
 	private ClientPlayNetworkAddon addon;
 
-	@Inject(method = "<init>", at = @At("RETURN"))
-	private void initAddon(CallbackInfo ci) {
+	@Override
+	public void lf$initAddon() {
 		this.addon = new ClientPlayNetworkAddon((ClientPlayNetworkHandler) (Object) this, this.client);
 		// A bit of a hack but it allows the field above to be set in case someone registers handlers during INIT event which refers to said field
 		ClientNetworkingImpl.setClientPlayAddon(this.addon);
