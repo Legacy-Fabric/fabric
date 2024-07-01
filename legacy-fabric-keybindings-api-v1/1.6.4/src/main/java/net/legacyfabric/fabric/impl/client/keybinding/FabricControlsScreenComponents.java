3fdba1fddd8ffcb21dd63429abd9c376d9bc57b4
Cat Core <arthur.koalas@free.fr>
2024-07-01 20:00:47 +0200

    Fix style

--- legacy-fabric-keybindings-api-v1/1.6.4/src/main/java/net/legacyfabric/fabric/impl/client/keybinding/FabricControlsScreenComponents.java
+++ legacy-fabric-keybindings-api-v1/1.6.4/src/main/java/net/legacyfabric/fabric/impl/client/keybinding/FabricControlsScreenComponents.java
@@ -1,12 +1,29 @@
+/*
+ * Copyright (c) 2020 - 2024 Legacy Fabric
+ * Copyright (c) 2016 - 2022 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.legacyfabric.fabric.impl.client.keybinding;
 
-import net.legacyfabric.fabric.api.util.Identifier;
+import java.util.function.Consumer;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 
-import java.util.function.Consumer;
+import net.legacyfabric.fabric.api.util.Identifier;
 
 public class FabricControlsScreenComponents {
 	private static final Identifier BUTTON_TEX = new Identifier("legacy-fabric", "textures/gui/creative_buttons.png");
@@ -40,24 +57,6 @@ public class FabricControlsScreenComponents {
 			this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
 			this.visible = extensions.fabric_isButtonVisible(type);
 			this.active = extensions.fabric_isButtonEnabled(type);
-
-//			if (this.visible) {
-//				int u = active && this.isHovered() ? 22 : 0;
-//				int v = active ? 0 : 10;
-//
-//				MinecraftClient minecraftClient = MinecraftClient.getInstance();
-////				minecraftClient.getTextureManager().bindTexture(new net.minecraft.util.Identifier(BUTTON_TEX.toString()));
-//
-//				GL11.glDisable(GL11.GL_LIGHTING);
-//				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-//
-//				this.drawTexture(this.x, this.y, u + (type == Type.NEXT ? 11 : 0), v, 11, 10);
-//
-////				if (this.hovered) {
-////					int pageCount = (int) Math.ceil((ItemGroup.itemGroups.length - AMOUNT_PER_PAGE) / 9D);
-////					gui.method_1128(I18n.translate("fabric.gui.creativeTabPage", extensions.fabric_currentPage() + 1, pageCount), mouseX, mouseY);
-////				}
-//			}
 		}
 	}
 
--- legacy-fabric-keybindings-api-v1/1.6.4/src/main/java/net/legacyfabric/fabric/mixin/client/keybinding/ControlsOptionsScreenMixin.java
+++ legacy-fabric-keybindings-api-v1/1.6.4/src/main/java/net/legacyfabric/fabric/mixin/client/keybinding/ControlsOptionsScreenMixin.java
@@ -1,31 +1,41 @@
-package net.legacyfabric.fabric.mixin.client.keybinding;
+/*
+ * Copyright (c) 2020 - 2024 Legacy Fabric
+ * Copyright (c) 2016 - 2022 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+package net.legacyfabric.fabric.mixin.client.keybinding;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-
 import com.llamalad7.mixinextras.sugar.Local;
-
-import net.legacyfabric.fabric.impl.client.keybinding.ControlsScreenExtensions;
-import net.legacyfabric.fabric.impl.client.keybinding.FabricControlsScreenComponents;
-
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
-
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.OptionButtonWidget;
-import net.minecraft.client.option.GameOptions;
-
-import net.minecraft.item.itemgroup.ItemGroup;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.OptionButtonWidget;
+import net.minecraft.client.option.GameOptions;
+
+import net.legacyfabric.fabric.impl.client.keybinding.ControlsScreenExtensions;
+import net.legacyfabric.fabric.impl.client.keybinding.FabricControlsScreenComponents;
 
 @Mixin(ControlsOptionsScreen.class)
 public class ControlsOptionsScreenMixin extends Screen implements ControlsScreenExtensions {
