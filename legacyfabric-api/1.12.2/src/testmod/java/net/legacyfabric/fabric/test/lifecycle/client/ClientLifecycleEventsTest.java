cda2928723fda77b0b4b7af22c9c00326ef6d385
Cat Core <arthur.koalas@free.fr>
2024-07-10 20:15:06 +0200

    Merge 1.12.2 test mods

---
+++ legacyfabric-api/1.12.2/src/testmod/java/net/legacyfabric/fabric/test/lifecycle/client/ClientLifecycleEventsTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 - 2024 Legacy Fabric
+ * Copyright (c) 2016 - 2022 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.legacyfabric.fabric.test.lifecycle.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import net.legacyfabric.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.legacyfabric.fabric.api.logger.v1.Logger;
+import net.legacyfabric.fabric.impl.logger.LoggerImpl;
+
+public class ClientLifecycleEventsTest implements ClientModInitializer {
+	private static final Logger LOGGER = Logger.get(LoggerImpl.API, "Test", "ClientLifecycleEvents");
+
+	@Override
+	public void onInitializeClient() {
+		ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+			LOGGER.info("Client started");
+		});
+		ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+			LOGGER.info("Client stopping");
+		});
+	}
+}
