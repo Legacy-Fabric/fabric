3590e54083cededce0b0e2653846b9e2db15240a
Cat Core <arthur.koalas@free.fr>
2025-02-10 23:15:51 +0100

    Fix compiling errors

--- legacy-fabric-block-api-v1/1.12.2/src/main/java/net/legacyfabric/fabric/mixin/block/versioned/ItemAccessor.java
+++ legacy-fabric-block-api-v1/1.12.2/src/main/java/net/legacyfabric/fabric/mixin/block/versioned/ItemAccessor.java
@@ -22,6 +22,8 @@ import java.util.Map;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 
 @Mixin(Item.class)
 public interface ItemAccessor {
