d949f2b9a7211acd62cfb11a78d4dc0d30bb7ccc
Cat Core <arthur.koalas@free.fr>
2026-02-11 23:25:12 +0100

    Fix style and code organization

--- legacy-fabric-resource-loader-v1/1.8.9/src/main/java/net/legacyfabric/fabric/mixin/resource/loader/client/MinecraftMixin.java
+++ legacy-fabric-resource-loader-v1/1.8.9/src/main/java/net/legacyfabric/fabric/mixin/resource/loader/client/MinecraftMixin.java
@@ -1,16 +1,32 @@
+/*
+ * Copyright (c) 2020 - 2026 Legacy Fabric
+ * Copyright (c) 2016 - 2022 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.legacyfabric.fabric.mixin.resource.loader.client;
-
-import net.legacyfabric.fabric.impl.resource.loader.ItemModelRegistryImpl;
-
-import net.minecraft.client.Minecraft;
-
-import net.minecraft.client.render.entity.ItemRenderer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.render.entity.ItemRenderer;
+
+import net.legacyfabric.fabric.impl.resource.loader.ItemModelRegistryImpl;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
